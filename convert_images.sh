9c560f71e46600be7fcab823ce0a384078fcd4ef
Deven Blake <trinity@trinity.moe>
2022-06-04 05:17:41 -0400

    2022-06-07

--- convert_images.sh
+++ convert_images.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -ex
 
+_convert(){
+	convert "$1" "$(printf "%s\n" "$1" | cut -d . -f 1).$2"
+}
+
 for file in *; do
 	if \
 			! printf "%s\n" "$file" | grep "\.png*" >/dev/null 2>&1 \
 			&& ! test -e "$(printf "%s\n" "$file" | cut -d . -f 1).png"
 		then
-		[ "$(printf "%s\n" "$file" | cut -d . -f 2)" = jpg ] \
-			&& convert "$file" "$(printf "%s\n" "$file" | cut -d . -f 1).png"
-		fi
+		case "$(printf "%s\n" "$file" | cut -d . -f 2)" in
+		jpg) _convert "$file" png ;;
+		webp) _convert "$file" png ;;
+		esac
+	fi
 done
